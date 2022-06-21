7011ce4eec1d8ac613ef8128301d04b5c8aaedf9
PopovGitProjects <popov.workmail@gmail.com>
2022-06-21 17:38:12 +0500

    Добавил логику в адаптеры и базу.

--- app/src/main/java/com/example/roomtestproject/adapter/NoteAdapter.kt
+++ app/src/main/java/com/example/roomtestproject/adapter/NoteAdapter.kt
@@ -1,4 +1,40 @@
 package com.example.roomtestproject.adapter
 
-class NoteAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.roomtestproject.R
+import com.example.roomtestproject.databinding.ItemLayoutBinding
+import com.example.roomtestproject.models.NoteModel
+
+class NoteAdapter: RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+    private var listNote = emptyList<NoteModel>()
+
+    class NoteViewHolder(view: View): RecyclerView.ViewHolder(view){
+        private val binding = ItemLayoutBinding.bind(view)
+        fun bind(item: NoteModel) = with(binding){
+            itemTitle.text = item.title
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_layout, parent, false)
+        return NoteViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        holder.bind(listNote[position])
+    }
+
+    override fun getItemCount(): Int {
+        return listNote.size
+    }
+    fun setList(list: List<NoteModel>){
+        listNote = list
+        notifyDataSetChanged()
+    }
 }
--- app/src/main/java/com/example/roomtestproject/db/NoteDataBase.kt
+++ app/src/main/java/com/example/roomtestproject/db/NoteDataBase.kt
@@ -1,4 +1,29 @@
 package com.example.roomtestproject.db
 
-class NoteDataBase {
+import android.content.Context
+import android.provider.DocumentsContract
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.roomtestproject.db.dao.NoteDao
+import com.example.roomtestproject.models.NoteModel
+
+@Database(entities = [NoteModel::class], version = 1)
+abstract class NoteDataBase: RoomDatabase() {
+    abstract fun getNoteDao(): NoteDao
+
+    companion object{
+        private var database: NoteDataBase ?= null
+
+        @Synchronized
+        fun getInstance(context: Context): NoteDataBase{
+            return if (database == null){
+                database = Room.databaseBuilder(context, NoteDataBase::class.java, "db")
+                    .build()
+                database as NoteDataBase
+            }else{
+                database as NoteDataBase
+            }
+        }
+    }
 }
